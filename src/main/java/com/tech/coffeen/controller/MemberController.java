fe74676ec878305afea6c9867c085c49dca1350f
mhwa9191 <mmhhwa@gmail.com>
2023-03-09 17:48:35 +0900

    mybatis-sett

--- src/main/java/com/tech/coffeen/controller/MemberController.java
+++ src/main/java/com/tech/coffeen/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.tech.coffeen.controller;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.tech.coffeen.service.CoffeeServiceInerface;
@@ -7,4 +9,7 @@ import com.tech.coffeen.service.CoffeeServiceInerface;
 @Controller
 public class MemberController {
 	CoffeeServiceInerface coffeeServiceInerface;
+	
+	@Autowired
+	private SqlSession sqlSession;
 }
