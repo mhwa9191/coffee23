ed5cc50a8ce92381d1d64e279bc4412a23e7c324
mhwa9191 <mmhhwa@gmail.com>
2023-03-09 18:01:42 +0900

    mybatis-sett+sqlsession+mapper

--- src/main/java/com/tech/coffeen/controller/MemberController.java
+++ src/main/java/com/tech/coffeen/controller/MemberController.java
@@ -3,13 +3,23 @@ package com.tech.coffeen.controller;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tech.coffeen.service.CoffeeServiceInerface;
 
 @Controller
+@RequestMapping("/member")
 public class MemberController {
 	CoffeeServiceInerface coffeeServiceInerface;
 	
 	@Autowired
 	private SqlSession sqlSession;
+	
+	@RequestMapping("/joinform")
+	public String joinform(Model model) {
+		System.out.println("--*--MemberController/joinform--*--");
+		return "/member/joinform";
+	}
+	
 }
---
+++ src/main/java/com/tech/coffeen/dao/Member.java
@@ -0,0 +1,5 @@
+package com.tech.coffeen.dao;
+
+public class Member {
+
+}
