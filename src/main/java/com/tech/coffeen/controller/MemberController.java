866da6e3c19574800f2a9101e7aa32b3fe88919d
mhwa9191 <mmhhwa@gmail.com>
2023-03-09 17:41:52 +0900

    controller+interface_member

---
+++ src/main/java/com/tech/coffeen/controller/MemberController.java
@@ -0,0 +1,10 @@
+package com.tech.coffeen.controller;
+
+import org.springframework.stereotype.Controller;
+
+import com.tech.coffeen.service.CoffeeServiceInerface;
+
+@Controller
+public class MemberController {
+	CoffeeServiceInerface coffeeServiceInerface;
+}
---
+++ src/main/java/com/tech/coffeen/service/CoffeeServiceInerface.java
@@ -0,0 +1,7 @@
+package com.tech.coffeen.service;
+
+import org.springframework.ui.Model;
+
+public interface CoffeeServiceInerface {
+	public void execute(Model model);
+}
